7a34ce0e0709595609cd4be3e49f7e64dc9d6376
Ed Jones <edjones@openhistoryproject.org>
2012-02-09 13:22:36 -0500

    Styling projects form, requests, responses.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -44,7 +44,9 @@ class ResponsesController < ApplicationController
 
     respond_to do |format|
       if @response.save
-        format.html { redirect_to @response, notice: 'Response was successfully created.' }
+        #format.html { redirect_to @response, notice: 'Response was successfully created.' }
+		logger.debug("##################################{ root_url }")
+		redirect_to(:back, :notice => 'Response was successfully created.' )
         format.json { render json: @response, status: :created, location: @response }
       else
         format.html { render action: "new" }
