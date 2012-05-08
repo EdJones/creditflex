5bd18f1d5b47da67a5f5b6160ca0f7e052e35854
Ed Jones <edjones@openhistoryproject.org>
2012-05-08 15:32:25 -0400

    Add sendgrid config.
    Signed-off-by: Ed Jones <edjones@openhistoryproject.org>

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Creditflex::Application.initialize!
+
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+:user_name => "app2792492@heroku.com",
+:password => "regan21",
+:domain => "OhiocreditFlexibility.org",
+:address => "smtp.sendgrid.net",
+:port => 587,
+:authentication => :plain,
+:enable_starttls_auto => true
+}
+
+config.action_mailer.raise_delivery_errors = true
