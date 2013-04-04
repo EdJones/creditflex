ef179be0cccf64adb13ebdc60a0751d36073b5ab
Ed Jones <edjones@openhistoryproject.org>
2013-04-04 11:39:37 -0400

    Echo popup working except for final render.
    Signed-off-by: Ed Jones <edjones@openhistoryproject.org>

--- app/models/echo.rb
+++ app/models/echo.rb
@@ -1,3 +1,4 @@
 class Echo < ActiveRecord::Base
   attr_accessible :user_id, :wish_id
+  belongs_to :wish
 end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -2,4 +2,5 @@ class Wish < ActiveRecord::Base
   attr_accessible :user_id, :wish
   belongs_to :user
   belongs_to :challenge
+  has_many :echoes
 end
