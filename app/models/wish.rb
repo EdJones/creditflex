77153a09ee6ee1cf7d83024fb393f0eeb0870b0b
Ed Jones <edjones@openhistoryproject.org>
2013-05-14 15:09:45 -0400

    Adjustment to wish_stream on index

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -9,11 +9,11 @@ end
 public
 def wish_stream
 
-  @wishes = Wish.order("created_at desc").limit(200).reverse
-  @echoes = Echo.order("created_at desc").limit(200).reverse
+  @wishes = Wish.order("created_at desc").limit(200)
+  @echoes = Echo.order("created_at desc").limit(200)
   
   @rewishes = @echoes.collect{|x| x.wish} + @wishes
-  @rewishes = @rewishes.sort_by { |obj| obj.created_at }.reverse
+  @rewishes = @rewishes.sort_by { |obj| obj.created_at }.reverse!
   
   
 
