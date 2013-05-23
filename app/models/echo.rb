fe44e2389738ebd3f944ec163de05423094e85a4
Ed Jones <edjones@openhistoryproject.org>
2013-05-23 19:07:29 -0400

    Fix the issue with sorting the wishes and echoes. Sort of. Until a decorator makes things reasonably efficient. Per Leon.
    Signed-off-by: Ed Jones <edjones@openhistoryproject.org>

--- app/models/echo.rb
+++ app/models/echo.rb
@@ -1,4 +1,5 @@
 class Echo < ActiveRecord::Base
   attr_accessible :user_id, :wish_id
   belongs_to :wish
+  belongs_to :user
 end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -3,18 +3,24 @@ class Wish < ActiveRecord::Base
   belongs_to :user
   belongs_to :challenge
   has_many :echoes
-end
-
 
 public
-def wish_stream
+def self.wish_stream
 
   @wishes = Wish.order("created_at desc").limit(100)
-  @echoes = Echo.order("created_at desc").limit(50)
+  @echoes = Echo.order("created_at desc").limit(50).map do |echo|
+	OpenStruct.new(created_at: echo.created_at, wish: echo.wish.wish, user: echo.wish.user, echoed_by: echo.user, echo_count: echo.wish.echoes.count)
+  end
   
-  @rewishes = @echoes.collect{|x| x.wish} + @wishes
-  @rewishes = @rewishes.sort_by { |obj| obj.created_at }.reverse!
+  # @rewishes = @echoes.collect{|x| x.wish} + @wishes
+  # @rewishes = @rewishes.sort_by { |obj| obj.created_at }.reverse!
   
-  
+  ( @echoes + @wishes ).sort_by(&:created_at).reverse!
+
+end
+
+def echo_count
+	echoes.count
+end
 
 end
