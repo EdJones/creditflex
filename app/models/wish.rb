17c7dc3c945c6b5d28ae5c421e7e4b9eeffda6db
Ed Jones <edjones@openhistoryproject.org>
2013-05-23 11:20:27 -0400

    Reposition content on Wishes page. Limit number of wishes fetched.
    Signed-off-by: Ed Jones <edjones@openhistoryproject.org>

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -9,8 +9,8 @@ end
 public
 def wish_stream
 
-  @wishes = Wish.order("created_at desc").limit(200)
-  @echoes = Echo.order("created_at desc").limit(200)
+  @wishes = Wish.order("created_at desc").limit(100)
+  @echoes = Echo.order("created_at desc").limit(50)
   
   @rewishes = @echoes.collect{|x| x.wish} + @wishes
   @rewishes = @rewishes.sort_by { |obj| obj.created_at }.reverse!
